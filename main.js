74b245ada62d8dec9ffec97d0ef572e41ca527ff
Squishy177 <2026peted05@moundsviewschools.org>
2024-11-06 13:49:22 -0600

    Added test research

--- main.js
+++ main.js
@@ -10113,7 +10113,13 @@ Game.Launch=function()
 			Game.Unlock('Elder Covenant');
 			Game.CollectWrinklers();
 			Game.storeToRefresh=1;
+			Game.SetResearch('Sentience I');
 		});
+		new Game.Upgrade('Sentience I',loc("Each %1 gains <b>+%2 base CpS per %3</b>.",[loc("You"),'<span>500 billion</span>2',loc("Javascript console")])+'<div class="warning">'+loc("Grants your clones a portion of sentience. Now they can make some descisions for themselves...")+'</div><q>This may not be the best idea</q>',8000000000000000000000000000000,[18,11],function(){Game.SetResearch('Sentience II');Game.storeToRefresh=1;});Game.last.pool='tech';
+		new Game.Upgrade('Sentience II',loc("Each %1 gains <b>+%2 base CpS per %3</b>.",[loc("You"),'<span>1 trillion</span>2',loc("Idleverse")])+'<div class="warning">'+loc("Grants your clones partial sentience. Now they can make most descisions for themselves...")+'</div><q>This is not the best idea</q>',16000000000000000000000000000000,[19,11],function(){Game.SetResearch('Sentience III');Game.storeToRefresh=1;});Game.last.pool='tech';
+		new Game.Upgrade('Sentience III',loc("Each %1 gains <b>+%2 base CpS per %3</b>.",[loc("You"),'<span>1 trillion</span>2',loc("Cortex baker")])+'<div class="warning">'+loc("Grants your clones a complete sentience. Now they can make all descisions for themselves...")+'</div><q>This is a terrible idea</q>',32000000000000000000000000000000,[20,11],function(){Game.SetResearch('Heavenly energy');Game.storeToRefresh=1;});Game.last.pool='tech';
+		new Game.Upgrade('Heavenly energy',loc("Your now sentient clones are requesting access to some of the heavenly power you obtained previously. Do you accept?")+'<div class="warning">'+loc("Your clones are starting an uprising. Regardless, this is a terrible idea.")+'</div><q>Curiousity</q>',1,[17,11],function(){Game.storeToRefresh=1;});Game.last.pool='tech';
+
 		Game.getPledgeDuration=function(){return Game.fps*60*(Game.Has('Sacrificial rolling pins')?60:30);}
 		Game.last.pool='toggle';
 		Game.last.displayFuncWhenOwned=function(){return '<div style="text-align:center;">'+loc("Time remaining until pledge runs out:")+'<br><b>'+Game.sayTime(Game.pledgeT,-1)+'</b></div>';}
