51eb854e34bf91054584288ad6541ee124151539
Squishy177 <2026peted05@moundsviewschools.org>
2024-11-07 08:26:58 -0600

    testing 0.2

--- minigameCaverns.js
+++ minigameCaverns.js
@@ -10,9 +10,7 @@ M.launch=function()
 		var str='';
 		str+='<style>'+
 		'#cavernBG{background:url('+Game.resPath+'img/shadedBorders.png),url('+Game.resPath+'img/BGcavern.png);background-size:100% 100%,auto;position:absolute;left:0px;right:0px;top:0px;bottom:16px;height:100px;overflow-y:scroll;}'+
-		'#templeContent{position:relative;box-sizing:border-box;padding:4px 24px;text-align:center;}'+
-		'.templeIcon{pointer-events:none;margin:12px 6px 0px 6px;width:48px;height:48px;opacity:0.8;position:relative;}'+
-		
+		'#cavernContent{position:relative;box-sizing:border-box;padding:4px 24px;text-align:center;}'+
 
 		
 		
