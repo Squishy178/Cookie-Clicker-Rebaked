82ee9d8d54939f7f3d18054f7c0e5bebc5c1924b
Squishy177 <2026peted05@moundsviewschools.org>
2026-02-14 10:22:10 -0600

    Node Generation refined!

--- minigameTrek.js
+++ minigameTrek.js
@@ -146,9 +146,8 @@ M.launch=function()
 				desc:'A reinforced ship front that plows through obstacles. Able to plow through asteroids with taking minimal damage.',
 			},
 		};
-		M.currentSetup="000"; //thruster, weapon, gadget, in order
+		M.currentSetup="0000"; //thruster, weapon 1 and 2, and gadget, in order
 		M.currentPlanet='earth';
-		M.
 		M.armor = 100;
 		M.fuel = 100;
 		M.maxFuel = 100;
@@ -156,9 +155,59 @@ M.launch=function()
 
 		M.map = [];
 		M.links = [];
+		M.distance=function(x1,y1,x2,y2){
+			return Math.sqrt((x1-x2)^2+(y1-y2)^2);
+		}
+		M.relaxNodes=function(iterations,minDistance){
+			let unlinkedForce = 0.05;
+			let linkedForce = 0.05;
+			let targetDistance = minDistance;
+			for (var iter=0;iter<iterations;iter++){
+				for (var i=0;i<M.map.length();i++){
+					for (var ii=i+1;ii<M.map.length();ii++){
+						let node1 = M.map[i];
+						let node2 = M.map[ii];
+						let distance = M.distance(node1.x,node1.y,node2.x,node2.y);
+						if (distance < targetDistance && distance>0){
+							let nx = (node2.x-node1.x)/distance;
+							let ny = (node2.y-node1.y)/distance;
+
+							let force = unlinkedForce * (targetDistance - distance);
+							let force1 = force * (node1.parent?0.5:1); // Parent node = Less affected
+							let force2 = force * (node2.parent?0.5:1);
+							node1.x += nx * force1;
+							node1.y += ny * force1;
+							node2.x -= nx * force2;
+							node2.y -= ny * force2;
+						}
+					}
+				}
+				for (var link of M.links){
+					let node1 = M.map[link.from];
+					let node2 = M.map[link.to];
+					let distance = M.distance(node1.x,node1.y,node2.x,node2.y);
+					if (distance > 0){
+						let nx = (node2.x-node1.x)/distance;
+						let ny = (node2.y-node1.y)/distance;
+
+						let force = linkedForce * (distance - targetDistance);
+						let force1 = force * (node1.parent?0.5:1); // Parent node = Less affected
+						let force2 = force * (node2.parent?0.5:1);
+						node1.x -= nx * force1;
+						node1.y -= ny * force1;
+						node2.x += nx * force2;
+						node2.y += ny * force2;
+					}
+				}
+			}
+		}
 		M.newMap=function(segementCount){
 			M.map = [];
 			M.links = [];
+			let minDistance = 50;
+			let branchChance = 0.5;
+			let maxBranches = 3;
+
 			//generate a new map
 			/*
 			This was very difficult to code, so here's an explanation of how it works:
@@ -186,14 +235,40 @@ M.launch=function()
 					type = choose(options);
 					links.push({from:i-1,to:i});
 				}
-				
-				M.map.push({type:type,x:x,y:y,visited:false});
+				M.map.push({type:type,x:x,y:y,visited:false,parent:true});
 			}
-			for (var i=0;i<segementCount;i++){
-				for (var ii=0;ii<segementCount;ii++){
-					if (Math.random()*20+40>Math.sqrt((M.map[i].x-M.map[ii].x)^2+(M.map[i].y-M.map[ii].y)^2)) links.push({from:ii,to:i});
+			//Branching nodes
+			for (var i=1;i<M.map.length();i++){
+				let parentNode = M.map[i];
+				if (Math.random()<branchChance){
+					let branches = Math.floor(Math.random()*maxBranches)+1;
+					for (var b=0;b<branches;b++){
+						let options = object.keys(M.planets[M.map[i-1]].parents);
+						for (var o in options){
+							let option = M.planets[o];
+							if (i<M.planets[option].min_distance) options.splice(option,1); //min distance
+						};
+					
+						type = choose(options);
+						M.map.push({type:type,x:parentNode.x + Math.random()*2 - 1,y:parentNode.y+Math.random()*2 - 1,visited:false,parent:false});
+						links.push({from:M.map.length(),to:i});
+					
+						
+					}
 				}
 			}
+			M.relaxNodes(150,minDistance);
+			// Link close nodes
+			for (var i; i<M.map.length();i++){
+				for (var ii=0; ii<M.map.length();ii++){
+					let node1 = M.map[i];
+					let node2 = M.map[ii];
+					if (i!=ii && Math.random()<0.7 && M.distance(node1.x,node1.y,node2.x,node2.y)<minDistance && !links.some(l=> (l.from==i && l.to==ii) || (l.from==ii && l.to==i))){
+						links.push({from:i,to:ii});
+					}
+				}
+			}
+			M.relaxNodes(50,minDistance);
 
 		};
 
@@ -208,6 +283,7 @@ M.launch=function()
 		
 		str+='</div>';
 		div.innerHTML=str;
+		M.newMap(10);
     }
 	M.logic=function()
 	{
@@ -219,5 +295,6 @@ M.launch=function()
 		//run each draw frame
 		
 	}
+	M.init(l('rowSpecial'+M.parent.id));
 
 }
